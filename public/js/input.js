e1f0f3f503817e63de0bd74abdbf0794435f6a5f
Bobby Grace <johnrobertgracejr@gmail.com>
2014-03-03 00:10:45 -0500

    make site responsive. change fps.

--- public/js/input.js
+++ public/js/input.js
@@ -1,6 +1,6 @@
 function MicrophoneSample() {
   this.getMicrophoneInput();
-  this.fps = 30;
+  this.fps = 8;
   this.frameCount = 0;
   this.mouth = document.getElementsByClassName('glyph-mouth');
   this.eyes = document.getElementsByClassName('glyph-eye');
